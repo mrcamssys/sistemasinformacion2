6a36c7a0997186a77a3101dfbfd1d4c9dc5b4929
1022957746 <163366179+1022957746@users.noreply.github.com>
2024-03-13 20:38:09 -0500

    Add files via upload

--- mi primer proyecto/index.php
+++ mi primer proyecto/index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina prueba</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>pagina prueba</title>
 </head>
 <body>
-    <h1>mi primera pagina hibrida estudiante</h1>
+    <h1>mi primera pagina hibrida jhoan</h1>
     <hr>
 
     <?php
         $variable=0;
         echo "mi primer hola mundo con php";
 
-        //Aqui creo mis comentaerios, esto es un contador de 0 a 10
 
-        for($variable=0; $variable<=10; $variable++){
-            echo "contador: ".$variable;
+        //aqui creo mis comentarios hr es una lidea de lado a lado 
+
+        for($variable=0;$variable<=10;$variable++){
+            echo"contador: ".$variable;
         }
-            
         
-    ?>
-
+    ?> 
+    
+    
 </body>
 </html>
