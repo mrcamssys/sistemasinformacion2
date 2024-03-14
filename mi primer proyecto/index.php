8b5f4e5ef87b1ecb4846b57ed2565dd42831ae7e
JuanFC2 <163366113+JuanFC2@users.noreply.github.com>
2024-03-13 20:33:53 -0500

    Add files via upload

---
+++ mi primer proyecto/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina prueba</title>
+</head>
+<body>
+    <h1>mi primera pagina hibrida estudiante</h1>
+    <hr>
+
+    <?php
+        $variable=0;
+        echo "mi primer hola mundo con php";
+
+        //Aqui creo mis comentaerios, esto es un contador de 0 a 10
+
+        for($variable=0; $variable<=10; $variable++){
+            echo "contador: ".$variable;
+        }
+            
+        
+    ?>
+
+</body>
+</html>
